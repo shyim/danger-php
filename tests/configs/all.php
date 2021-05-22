f83d55e361dac7268cd6ecc3aafb545e3ea5650a
Soner Sayakci <s.sayakci@shopware.com>
2021-05-22 11:13:30 +0000

    fix(ci): Fix CheckPhpCsFixerRuleTest

--- tests/configs/all.php
+++ tests/configs/all.php
@@ -5,8 +5,8 @@ use Danger\Config;
 return
     (new Config())
         ->useRule(function (Danger\Context $context): void {
-        $context->failure('Test');
-        $context->warning('Test');
-        $context->notice('Test');
-    })
+            $context->failure('Test');
+            $context->warning('Test');
+            $context->notice('Test');
+        })
     ;
