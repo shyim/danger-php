a36834767c153992ea13ac9aff706866c71c5b70
tinect <s.koenig@tinect.de>
2021-05-25 22:00:20 +0200

    fix: detect correct pluralization in MaxCommitRule

--- src/Rule/MaxCommitRule.php
+++ src/Rule/MaxCommitRule.php
@@ -7,14 +7,19 @@ use Danger\Context;
 
 class MaxCommitRule
 {
-    public function __construct(private int $maxCommits = 1, private string $message = 'Please squash your commits to ###AMOUNT### commit(s) only')
+    public function __construct(private int $maxCommits = 1, private string $message = 'Please squash your commits to ###AMOUNT### only')
     {
     }
 
     public function __invoke(Context $context): void
     {
         if (count($context->platform->pullRequest->getCommits()) > $this->maxCommits) {
-            $context->failure(str_replace(['###AMOUNT###'], [$this->maxCommits], $this->message));
+            $message = $this->maxCommits . ' commits';
+            if ($this->maxCommits === 1) {
+                $message = 'one commit';
+            }
+
+            $context->failure(str_replace(['###AMOUNT###'], [$message], $this->message));
         }
     }
 }
