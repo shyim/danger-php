05390ec816cad1cba4cc70acfb06ddb51cecc92d
Shyim <s.sayakci@shopware.com>
2022-11-26 12:37:48 +0100

    fix: uff

--- src/Runner.php
+++ src/Runner.php
@@ -7,12 +7,9 @@ class Runner
 {
     public function run(Config $config, Context $context): void
     {
-        foreach ($config->getRules() as $rule) {
+        foreach($config->getRules() as $rule) {
             $rule($context);
         }
-        
-        
-        
 
         /**
          * Run after hooks. Can be used to set labels after all rules has been run
